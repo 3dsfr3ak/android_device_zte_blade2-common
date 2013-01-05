9da64550233d1d9ddf5a1e9bc93081e8fdcc74ce
Daz Jones <yuki@thebronasium.com>
2013-01-05 22:51:18 +0000

    TARGET_DEVICE -> TARGET_BOOTLOADER_NAME

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_DEVICE),blade2)
+ifeq ($(TARGET_BOOTLOADER_NAME),blade2)
 
 LOCAL_PATH := $(call my-dir)
 
